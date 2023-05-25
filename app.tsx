ce1311f43c42b1334188bdf51fabb3b3f71db8d2
Will <82029448+wjthieme@users.noreply.github.com>
2023-05-25 14:46:58 +0200

    First version of the web app (#3)
    * Initial version of web app
    
    * Fix

---
+++ app.tsx
@@ -0,0 +1,18 @@
+import "css:./app.css";
+import React from "react";
+import background from "url:./public/background.webp";
+import type { ReactElement } from "react";
+import { createRoot } from "react-dom/client";
+
+
+const Root = (): ReactElement => {
+
+    return (
+        <>
+            <img className="background" src={background} />
+            <span className="headline">IW</span>
+        </>
+    );
+};
+
+createRoot(document.getElementById("root") ?? new HTMLElement()).render(<Root />);
