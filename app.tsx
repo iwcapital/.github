f296eeb55d2d74fff97c3e5ee4ce31634e636ae3
Wilhelm Thieme <wjthieme@gmail.com>
2023-05-31 17:58:20 +0200

    Added checkout link popup page

--- app.tsx
+++ app.tsx
@@ -1,16 +1,42 @@
 import "css:./app.css";
-import React from "react";
+import React, { useMemo, useCallback, lazy, useEffect } from "react";
 import background from "url:./public/background.webp";
 import type { ReactElement } from "react";
 import { createRoot } from "react-dom/client";
 
+const Checkout = lazy(() => import("./checkout"));
 
 const Root = (): ReactElement => {
+    const [popupContent, setPopupContent] = React.useState<ReactElement | null>(null);
+
+    const closePopup = useCallback(() => {
+        const path = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        window.history.replaceState({ path }, "", path);
+        setPopupContent(null);
+    }, []);
+
+    const popup = useMemo(() => {
+        if (popupContent == null) { return; }
+        return (
+            <>
+                <div className="popup-overlay" onClick={closePopup} />
+                <div className="popup-background">{popupContent}</div>
+            </>
+        );
+    }, [popupContent]);
+
+    useEffect(() => {
+        const query = new URLSearchParams(window.location.search);
+        if (query.get("checkout") === "") {
+            setPopupContent(<Checkout />);
+        }
+    }, []);
 
     return (
         <>
             <img className="background" src={background} alt="" />
             <span className="headline">IW</span>
+            {popup}
         </>
     );
 };
